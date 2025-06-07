2c91bf5c229c4eddea3eb037beb7d021c4785e81
Terry <74259595+TechT360@users.noreply.github.com>
2025-06-07 15:08:51 +0100

    Add files via upload
    Password/pin generator; giving user the option to choose between password or pin and how long they want the password or pin to be

---
+++ password_gen2.py
@@ -0,0 +1,23 @@
+import string, secrets
+
+chrs = string.ascii_letters 
+numbers = string.digits
+
+choose = input("Password or pin? ")
+if choose == "password":
+    length = input("Choose your password length: ")
+elif choose == "pin":
+    length = input("Choose your pin length: ")
+else:
+    print("Invalid choice.")
+    exit()
+
+password_in_chrs = ''.join(secrets.choice(chrs + numbers) for i in range(int(length)))
+password_in_numbers = ''.join(secrets.choice(numbers) for i in range(int(length)))
+
+if choose == "password":
+    print(password_in_chrs)
+    
+if choose == "pin":
+    print(password_in_numbers)
+print("**********Python Password Generator**********")
